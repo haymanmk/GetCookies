95c82608d333e0ab193fe3e3bf62af02b5e82057
haymanmk <haymanmk@gmail.com>
2023-05-11 15:02:06 +0800

    set up message listener

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,28 @@
+chrome.runtime.onMessageExternal.addListener(
+  (request, sender, sendResponse) => {
+    if (request.openURL) {
+      chrome.tabs
+        .create({
+          active: false,
+          url: request.openURL,
+        })
+        .then(() => {
+          sendResponse(true);
+          console.log(`${request.openURL} opened.`);
+        })
+        .catch((err) => {
+          sendResponse(false, err);
+          console.log("ERROR", err);
+        });
+    } else if (request.getCookies) {
+      chrome.cookies.getAll({ domain: request.getCookies }, (res) => {
+        if (res) sendResponse(res);
+        else sendResponse(false, "Cannot get cookies.");
+      });
+    }
+  }
+);
+
 const cookies = chrome.cookies.getAll(
   { domain: "www.appbrewery.co" },
   (cookie) => {
